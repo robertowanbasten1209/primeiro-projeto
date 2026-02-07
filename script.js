132a5e4b43c04b91c0d5c0ddc0835749e0e34da2
Roberto Wanbasten <you@example.com>
2026-02-07 18:33:54 -0300

    adjustments and change background based on media queries

--- script.js
+++ script.js
@@ -14,9 +14,9 @@ function toggleMode() {
   //substituir a imagem
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem
-    img.setAttribute("src", "./assent/assets/avatar-light.png")
+    img.setAttribute("src", "./assent/avatar-light.png")
   } else {
     // se tiver sem light mode, mantar a imagem normal
-    img.setAttribute("src", "./assent/assets/Avatar.png")
+    img.setAttribute("src", "./assent/Avatar.png")
   }
 }
